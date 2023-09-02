ca9ac0674a7958c0868992968dacef31a0364f5e
Siye1234 <143892451+Siye1234@users.noreply.github.com>
2023-09-03 00:52:53 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+//imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+
+//database
+const db = mysql.createConnection({
+    host: 'sql8.freemysqlhosting.net',
+    user: 'sql8644076',
+    port:"3306",
+    password: '7xIUTgHi35',
+    database: 'sql8644076',
+  });
+
+//Express Router
+const router = express.Router();
+
+//Middleware
+router.use(bodyParser.json());
+router.use(cors()); 
+
+//Routes Management
+//Homepage
+//Homepage
+router.get("/", function (req, res) {
+    res.sendFile("views/index.html", { root: __dirname + "/../" });
+});
+
+
+
+
+router.post('/api/login', (req, res) => {
+  const receivedData = req.body;
+  console.log('Received data:', receivedData);
+
+  // Handle the data on the server as needed
+  console.log('Email:', receivedData.email);
+  console.log('Password:', receivedData.password);
+
+  // Send a response back to the client
+  res.status(200).json({ message: 'Data received on the server', data: receivedData });
+});
+
+router.post('/api/register', (req, res) => {
+  const receivedData = req.body;
+  console.log('Received data:', receivedData);
+
+  // Handle the data on the server as needed
+  console.log('Full Name:', receivedData.fullname);
+  console.log('Sutdent Email:', receivedData.email);
+  console.log('Password:', receivedData.password);
+
+  // Send a response back to the client
+  res.status(200).json({ message: 'Data received on the server', data: receivedData });
+});
+
+module.exports = router;
