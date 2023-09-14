e548cdf6147fd22dbed80bf36e8863b941577f9d
Tran203 <tshetshe203@gmail.com>
2023-09-14 21:41:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/", indexRoute);
 //app.use("/admin", adminRoute);
 
 // Start the server
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
